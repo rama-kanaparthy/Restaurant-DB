9d699f99c7ee98352d5f53f88a7def8bac0cfa99
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2025-03-28 03:04:24 +0530

    restaurant db initial code

---
+++ src/main/java/com/rama/restaurant_db/RestaurantDbApplication.java
@@ -0,0 +1,13 @@
+package com.rama.restaurant_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rama/restaurant_db/RestaurantDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rama.restaurant_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
